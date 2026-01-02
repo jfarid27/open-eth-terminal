b17028f452f27581b7a5fe6a8efb789334de0ee0
jrab227@gmail.com <jrab227@gmail.com>
2026-01-02 18:10:05 -0500

    Remove demo.

--- open_eth/spot/model/CoinGeckoApi.ts
+++ open_eth/spot/model/CoinGeckoApi.ts
@@ -5,8 +5,6 @@ import { ExchangeSymbol } from "../../types/symbols.ts";
 
 const COINGECKO_API = "https://api.coingecko.com/api/v3/simple/price";
 
-// https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin&x_cg_demo_api_key=CG-K6DJf3NW7XupuMyLpsE4kBeT
-    
 export type fetchSpotCoingeckoResponse = {
     symbol: ExchangeSymbol;
     price: number;
