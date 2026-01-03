1e563fd724f06e72cadb34d5d23613beab66560c
jrab227@gmail.com <jrab227@gmail.com>
2026-01-02 20:57:16 -0500

    Add base polymarket API calls.

---
+++ open_eth/prediction_markets/model/Polymarket.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+/**
+ * Fetches the list of available topic tags from Polymarket.
+ * @link https://docs.polymarket.com/api-reference/
+ */
+export async function fetchTopicsPolymarket() {
+    const response = await axios.get("https://gamma-api.polymarket.com/tags");
+    return response.data;
+}
+
+/**
+ * Fetches the list of available events from Polymarket.
+ * @param tagId The ID of the tag to fetch events for.
+ * @link https://docs.polymarket.com/api-reference/
+ */
+export async function fetchEventData(tagId: string) {
+    const response = await axios.get(
+        `https://gamma-api.polymarket.com/events`,
+        {
+            params: {
+                tag_id: tagId,
+            },
+        }
+    );
+    return response.data;
+}
+
+/**
+ * Fetches the list of available markets from Polymarket.
+ * @param tagId The ID of the tag to fetch markets for.
+ * @link https://docs.polymarket.com/api-reference/
+ */
+export async function fetchMarketData(tagId: string) {
+    const response = await axios.get(
+        `https://gamma-api.polymarket.com/markets`,
+        {
+            params: {
+                tag_id: tagId,
+            },
+        }
+    );
+    return response.data;
+}
