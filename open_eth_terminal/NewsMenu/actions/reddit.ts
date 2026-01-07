9777b50771de3dfbf662b292ee05d51179281a6d
jrab227@gmail.com <jrab227@gmail.com>
2026-01-07 03:26:56 -0500

    Add reddit search action.

--- open_eth_terminal/NewsMenu/actions/reddit.ts
+++ open_eth_terminal/NewsMenu/actions/reddit.ts
@@ -24,6 +24,55 @@ const generateRedditDataFromFeed = pipe(
 );
 
 /**
+ * Return top posts from the given search term
+ * @param {TerminalUserStateConfig} st Terminal user state 
+ * @param {string} query Query term to fetch posts from 
+ * @param {number} limit Number of posts to fetch 
+ * @returns {CommandState} 
+ */
+export const redditSearchTopHandler = (st: TerminalUserStateConfig)=>
+    async (query: string, limit: number): Promise<CommandState> => {
+
+        let _query = query;
+        if (!_query) {
+            console.log(chalk.red("No query term supplied."))
+            return {
+                result: { type: CommandResultType.Error },
+                state: st,
+            };
+        }
+        
+        try {
+            const feed = await news.reddit.search(query, limit || 20);
+            
+            const redditData = generateRedditDataFromFeed(feed.items);
+            
+            
+            for (const item of redditData) {
+                console.log(chalk.green(item.title))
+                console.log(chalk.blue(item.date) + " | " + chalk.green(item.author))
+                console.log(chalk.red(item.link) + "\n")
+            }
+            
+            return {
+                result: { type: CommandResultType.Success },
+                state: st,
+            };
+
+        } catch (error) {
+            console.log(chalk.red("An error occured fetching the feed."))
+            
+            if (st.logLevel) {
+                console.log(error)
+            }
+            return {
+                result: { type: CommandResultType.Error },
+                state: st,
+            };
+        }
+}
+
+/**
  * Return top posts from the given subreddit
  * @param {TerminalUserStateConfig} st Terminal user state 
  * @param {string} subreddit Subreddit to fetch posts from 
