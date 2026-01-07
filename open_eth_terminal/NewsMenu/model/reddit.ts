c31b7575e57bd90812e9387c51f032ac173c913c
jrab227@gmail.com <jrab227@gmail.com>
2026-01-07 03:26:18 -0500

    Add initial reddit search model.

--- open_eth_terminal/NewsMenu/model/reddit.ts
+++ open_eth_terminal/NewsMenu/model/reddit.ts
@@ -12,3 +12,17 @@ export async function getRedditBest(subreddit: string, limit: number=20) {
     const feed = await parser.parseURL(url);
     return feed;
 }
+
+/**
+ * Fetch top posts from the given query
+ * @param query Query to fetch posts from
+ * @param limit Number of posts to fetch
+ * @returns Feed object
+ */
+export async function getRedditSearchTop(query: string, limit: number=20) {
+    const url = `https://www.reddit.com/search/.rss?q=${query}&type=posts&sort=top&t=week&limit=${limit}`;
+        
+    const parser = new Parser();
+    const feed = await parser.parseURL(url);
+    return feed;
+}
